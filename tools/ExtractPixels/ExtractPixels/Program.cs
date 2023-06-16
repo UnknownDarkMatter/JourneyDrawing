60572bf8633900f1c4f262cfda288364b5be68f2
sebastien.bonelli <sebastien.bonelli@agap2-it.fr>
2023-06-16 19:45:15 +0200

    WIP : todo, revoir l'algo de traçage des borders

--- tools/ExtractPixels/ExtractPixels/Program.cs
+++ tools/ExtractPixels/ExtractPixels/Program.cs
@@ -56,51 +56,50 @@ using (var image = new Bitmap(Image.FromFile(inputPath)))
     int i = 1;//continent number
     int s = 1;//variable indicating progress when walking along the continents
 
-    var prod = false;
+    var prod = true;
     if (prod)
     {
-        //bordersTracing.DoTraceBorders(1847, 75, outputMap, i++, ref s);//continent eurasien
-        //bordersTracing.DoTraceBorders(973, 211, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(972, 220, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(991, 236, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1847, 75, outputMap, i++, ref s);//continent eurasien
+        bordersTracing.DoTraceBorders(973, 211, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(972, 220, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(991, 236, outputMap, i++, ref s);//
         bordersTracing.DoTraceBorders(904, 136, outputMap, i++, ref s);//angleterre
-        //bordersTracing.DoTraceBorders(875, 153, outputMap, i++, ref s);//irlande
-        //bordersTracing.DoTraceBorders(896, 248, outputMap, i++, ref s);//contient africain
+        bordersTracing.DoTraceBorders(875, 153, outputMap, i++, ref s);//irlande
+        bordersTracing.DoTraceBorders(896, 248, outputMap, i++, ref s);//contient africain
         bordersTracing.DoTraceBorders(1177, 497, outputMap, i++, ref s);//madagascar
-        //bordersTracing.DoTraceBorders(1657, 488, outputMap, i++, ref s);//australie
-        //bordersTracing.DoTraceBorders(1656, 157, outputMap, i++, ref s);//japon
-        //bordersTracing.DoTraceBorders(1655, 200, outputMap, i++, ref s);//japon
-        //bordersTracing.DoTraceBorders(1649, 219, outputMap, i++, ref s);//japon
-        //bordersTracing.DoTraceBorders(1608, 257, outputMap, i++, ref s);//japon
-        //bordersTracing.DoTraceBorders(1596, 258, outputMap, i++, ref s);//japon
-        //bordersTracing.DoTraceBorders(1524, 396, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1415, 403, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1470, 462, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1562, 390, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1545, 426, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1579, 426, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1539, 439, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1574, 448, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1528, 474, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1543, 474, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1538, 480, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1568, 476, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1568, 476, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1604, 434, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1693, 460, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1487, 330, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1543, 341, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1335, 386, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1677, 644, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1812, 611, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(1789, 671, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(822, 92, outputMap, i++, ref s);//islande
-        //bordersTracing.DoTraceBorders(748, 2, outputMap, i++, ref s);//groenland
-        //bordersTracing.DoTraceBorders(443, 339, outputMap, i++, ref s);//amerique du nord
-        //bordersTracing.DoTraceBorders(526, 392, outputMap, i++, ref s);//amerique du sud
-        //bordersTracing.DoTraceBorders(505, 313, outputMap, i++, ref s);//
-        //bordersTracing.DoTraceBorders(557, 329, outputMap, i++, ref s);//
-                                                           
+        bordersTracing.DoTraceBorders(1657, 488, outputMap, i++, ref s);//australie
+        bordersTracing.DoTraceBorders(1656, 157, outputMap, i++, ref s);//japon
+        bordersTracing.DoTraceBorders(1655, 200, outputMap, i++, ref s);//japon
+        bordersTracing.DoTraceBorders(1649, 219, outputMap, i++, ref s);//japon
+        bordersTracing.DoTraceBorders(1608, 257, outputMap, i++, ref s);//japon
+        bordersTracing.DoTraceBorders(1596, 258, outputMap, i++, ref s);//japon
+        bordersTracing.DoTraceBorders(1524, 396, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1415, 403, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1470, 462, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1562, 390, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1545, 426, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1579, 426, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1539, 439, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1574, 448, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1528, 474, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1543, 474, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1538, 480, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1568, 476, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1568, 476, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1604, 434, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1693, 460, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1487, 330, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1543, 341, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1335, 386, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1677, 644, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1812, 611, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(1789, 671, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(822, 92, outputMap, i++, ref s);//islande
+        bordersTracing.DoTraceBorders(748, 2, outputMap, i++, ref s);//groenland
+        bordersTracing.DoTraceBorders(443, 339, outputMap, i++, ref s);//amerique du nord
+        bordersTracing.DoTraceBorders(526, 392, outputMap, i++, ref s);//amerique du sud
+        bordersTracing.DoTraceBorders(505, 313, outputMap, i++, ref s);//
+        bordersTracing.DoTraceBorders(557, 329, outputMap, i++, ref s);//
     }
     else
     {
